da143dcad360b174f6d49b38476786519d98f952
Ludovic Barman <lbarman@protonmail.ch>
2017-07-02 21:51:44 +0200

    Updates db

--- parsejson.py
+++ parsejson.py
@@ -1,6 +1,7 @@
 from os import walk
 import json
 import operator
+import sys
 
 directory="jsondata"
 cards = []
@@ -8,63 +9,69 @@ editions = []
 editionUniqId = 1
 
 def getOrElse(mapVal, key, elseVal):
-	if key in mapVal:
-		return mapVal[key]
-	return elseVal
+    if key in mapVal:
+        return mapVal[key]
+    return elseVal
 
 
 def openFile(fileName):
-	global directory, cards, editions, editionUniqId
-	with open(directory + "/" + fileName) as json_data:
-		cardsData = json.load(json_data)
-		i = 0
-		while i<len(cardsData):
-			#print json.dumps(cardsData[i], sort_keys=True, indent=2, separators=(',', ': '))
-			newCard = {}
-			newCard["id"] = cardsData[i]["id"]
-			newCard["name"] = cardsData[i]["name"]
-			newCard["power"] = getOrElse(cardsData[i],"power",-1)
-			newCard["toughness"] = getOrElse(cardsData[i],"toughness",-1)
-			newCard["types"] = ",".join(cardsData[i]["types"])
-			newCard["subtypes"] = ",".join(getOrElse(cardsData[i],"subtypes",""))
-			newCard["text"] = cardsData[i]["text"]
-			newCard["store_url"] = cardsData[i]["store_url"]
-			newCard["colors"] = ",".join(getOrElse(cardsData[i],"colors",""))
-			newCard["cost"] = cardsData[i]["cost"]
-			newCard["cmc"] = cardsData[i]["cmc"]
-			newCard["editions"] = []
+    global directory, cards, editions, editionUniqId
+    with open(directory + "/" + fileName) as json_data:
+        cardsData = json.load(json_data)
 
-			for editionRaw in cardsData[i]["editions"]:
-				#print json.dumps(editionRaw, sort_keys=True, indent=2, separators=(',', ': '))
-				edition = {}
-				edition["id"] = editionUniqId
-				edition["set"] = editionRaw["set"]
-				edition["set_id"] = editionRaw["set_id"]
-				edition["artist"] = editionRaw["artist"]
-				edition["image_url"] = editionRaw["image_url"]
-				edition["rarity"] = editionRaw["rarity"]
+        i = 0
+        while i<len(cardsData):
+            #print json.dumps(cardsData[i], sort_keys=True, indent=2, separators=(',', ': '))
+            newCard = {}
+            newCard["id"] = cardsData[i]["id"]
+            newCard["name"] = cardsData[i]["name"]
+            newCard["power"] = getOrElse(cardsData[i],"power",-1)
+            newCard["toughness"] = getOrElse(cardsData[i],"toughness",-1)
+            newCard["types"] = ",".join(cardsData[i]["types"])
+            newCard["subtypes"] = ",".join(getOrElse(cardsData[i],"subtypes",""))
+            newCard["text"] = cardsData[i]["text"]
+            newCard["store_url"] = cardsData[i]["store_url"]
+            newCard["colors"] = ",".join(getOrElse(cardsData[i],"colors",""))
+            newCard["cost"] = cardsData[i]["cost"]
+            newCard["cmc"] = cardsData[i]["cmc"]
+            newCard["editions"] = []
 
-				#store in editions
-				editions.append(edition)
+            for editionRaw in cardsData[i]["editions"]:
+                #print json.dumps(editionRaw, sort_keys=True, indent=2, separators=(',', ': '))
+                edition = {}
+                edition["id"] = editionUniqId
+                edition["set"] = editionRaw["set"]
+                edition["set_id"] = editionRaw["set_id"]
+                edition["artist"] = editionRaw["artist"]
+                edition["image_url"] = editionRaw["image_url"]
+                edition["rarity"] = editionRaw["rarity"]
 
-				#link to our card
-				newCard["editions"].append(editionUniqId)
-				editionUniqId += 1
+                #store in editions
+                editions.append(edition)
 
-			#store in cards
-			cards.append(newCard)
-			i+=1
+                #link to our card
+                newCard["editions"].append(editionUniqId)
+                editionUniqId += 1
+
+            #store in cards
+            cards.append(newCard)
+            i+=1
+
+            print newCard["editions"]
+            sys.exit(1)
 
 # list all files in dir
 files = []
 for (dirpath, dirnames, filenames) in walk(directory):
-	files = filenames
-	break
+    files = filenames
+    break
 
 for file in files:
-	print "Processing "+file
-	openFile(file)
-	break
+    print "Processing "+file
+    openFile(file)
+    break
+
+sys.exit(1)
 
 sqlOut = ""
 sqlOut += "CREATE TABLE `cards` (\n"
@@ -84,24 +91,25 @@ sqlOut += ")\n\n"
 
 
 for card in cards:
-	sqlLine = "INSERT INTO `cards` ("
-	sortedCard = sorted(card.items(), key=operator.itemgetter(0))
-	fields = []
-	for field in sortedCard:
-		fields.append(field[0])
-	sqlLine += ",".join(fields)+") VALUES ("
-	data = []
-	for field in sortedCard:
-		if type(field[1]) is unicode or type(field[1]) is str:
-			data.append("\""+(field[1]).encode('utf-8').strip().replace("\"", "\\\"")+"\"")
-		elif type(field[1]) is int:
-			data.append(""+str(field[1])+"")
-		elif type(field[1]) is list:
-			data.append("\""+(",".join(map(str,field[1])))+"\"")
-		else:
-			print "Unknown field"+str(type(field[1]))
-			os.exit(1)
-	sqlLine += ",".join(data)+");\n"
-	sqlOut += sqlLine
+    sqlLine = "INSERT INTO `cards` ("
+    sortedCard = sorted(card.items(), key=operator.itemgetter(0))
+    fields = []
+    for field in sortedCard:
+        print "Field:", field[0], field[1] 
+        fields.append(field[0])
+    sqlLine += ",".join(fields)+") VALUES ("
+    data = []
+    for field in sortedCard:
+        if type(field[1]) is unicode or type(field[1]) is str:
+            data.append("\""+(field[1]).encode('utf-8').strip().replace("\"", "\\\"")+"\"")
+        elif type(field[1]) is int:
+            data.append(""+str(field[1])+"")
+        elif type(field[1]) is list:
+            data.append("\""+(",".join(map(str,field[1])))+"\"")
+        else:
+            print "Unknown field"+str(type(field[1]))
+            os.exit(1)
+    sqlLine += ",".join(data)+");\n"
+    sqlOut += sqlLine
 
 print sqlOut
